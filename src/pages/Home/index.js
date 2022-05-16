84a35284f6c993506bf3a099812c7a98a5e7fccd
Cauã Mendes <cauamgomes6@gmail.com>
2022-05-16 13:38:23 -0300

    Organizei as pastas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,9 @@ import './index.scss';
 export default function home() {
     return(
         <main className='page-home'>
-
+            <p>
+                oiii
+            </p>
         </main>
     )
 }
