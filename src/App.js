68763be1e4846ca59fef793a8d1a1426e6956ae9
Davi Moreira <davimoreira7891@gmai.com>
2022-06-03 10:45:56 -0300

    aaa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Link to="/home">Home</Link>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import App from './App';
+import Cadastro from './pages/Cadastro/index';
+import Home from './pages/Home/index';
+import Form from './pages/Form/index';
+import Adm from './pages/Adm/index';
+import Login from './pages/Login/index';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>} /> 
+        <Route path='/form' element={<Form/>} /> 
+        <Route path='/login' element={<Login/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/adm' element={<Adm/>} />
+        <Route path='/cadastro' element={<Cadastro/>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
