af679258ab83a84313d76f484036651940633434
Eric <ets1603@gmail.com>
2022-06-10 01:17:43 -0300

    react-login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,40 @@
+
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+import { useState } from 'react'
+
 import './index.scss';
 import { Link } from 'react-router-dom';
 
 
-export default function login() {
+export default function IIndex() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState('');
+
+    const navigate= useNavigate();
+
+    async function entrarClick() {
+        try { 
+            const t = await axios.post('http://localhost:5000/login', {
+            emailo: email,
+            senha: senha
+        });
+
+            navigate('/adm');
+        
+        } catch (err) {
+            if (err.response.status === 401) {
+
+                setErro(err.response.data.erro);
+
+            }
+        
+        }  
+       
+    }
+
     return(
     <main className="Main">
         
@@ -18,18 +50,18 @@ export default function login() {
                         <div className="espac4"></div>
                     </div>
                     
-                    <input type="text" className="input-usuario" name="usuario"/>
+                    <input type="text" className="input-usuario" name="usuario" value={email} onChange={e => setEmail(e.target.value) }  />
                    
                     <div className="Div-Label">
                         <label for="usuario" className='senha'> SENHA : </label>
                         
                     </div>
                     
-                    <input type="password" className="input-usuario" name="usuario"/>
+                    <input type="password" className="input-usuario" name="usuario" value={senha} onChange={e => setSenha(e.target.value) } />
                     
                 </div>
                 <div className="Div-Button">
-                    <Link to='../adm'><button className="botton">ENTRAR</button></Link> 
+                    <Link to='../adm'><button className="botton" onClick={entrarClick} >ENTRAR</button></Link> 
                 </div>
 
                 
